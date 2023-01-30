ba9a8c78fd695b990907fe1c476c631fdfeb8c89
nduongw <duong5112001@gmail.com>
2023-01-30 12:06:49 +0700

    add login

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_09_050951) do
+ActiveRecord::Schema.define(version: 2023_01_10_090231) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.integer "number_of_pages"
+	@@ -19,4 +19,16 @@
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
